4e533c5e35aa113098c0e5477e78e9e0ef710ef1
Zongheng Yang <zongheng.y@gmail.com>
2018-06-19 09:54:02 -0700

    Make DatasetGenerater use a seeded random (generate deterministic runs)

--- src/main/java/edu/berkeley/riselab/rlqopt/workload/DatasetGenerator.java
+++ src/main/java/edu/berkeley/riselab/rlqopt/workload/DatasetGenerator.java
@@ -27,7 +27,22 @@ public class DatasetGenerator {
   private Database db;
   private TableStatisticsModel ts;
 
+  private Random random;
+
+  public void describe() {
+    int numRelations = db.size();
+    System.out.print("Num relations: " + numRelations);
+    System.out.print("; #Attrs: ");
+    for (Relation r : db) {
+        System.out.print(r.size() + " ");
+    }
+    int totalNumAttrs = db.getAllAttributes().size();
+    System.out.println("; Total #attrs: "+ totalNumAttrs);
+  }
+
   public DatasetGenerator(int numRelations, int numAttributes, int maxTableSize, int histogramRes) {
+    // Change this seed to get different runs.
+    this.random = new Random(1234);
 
     this.numAttributes = numAttributes;
     attrTypes = generateAttributes();
@@ -45,86 +60,84 @@ public class DatasetGenerator {
     ts.setJoinReductionFactors(getJoinReductionFactors(db));
   }
 
-  private String int2bin(int x, int len){
+  private String int2bin(int x, int len) {
     String binCode = Integer.toBinaryString(x);
-    while (binCode.length() < len)
-      binCode = "0" + binCode;
+    while (binCode.length() < len) binCode = "0" + binCode;
 
     return binCode;
   }
 
+  private HashMap<HashSet<Relation>, Double> getJoinReductionFactors(
+      LinkedList<Relation> allRelations) {
+    HashMap<HashSet<Relation>, Double> reductions = new HashMap();
 
-  private HashMap<HashSet<Relation>, Double> getJoinReductionFactors(LinkedList<Relation> allRelations){
-      HashMap<HashSet<Relation>, Double> reductions = new HashMap();
+    int maxCode = (int) Math.pow(2, allRelations.size());
 
-      int maxCode = (int) Math.pow(2, allRelations.size());
+    // System.out.println(maxCode);
 
-      //System.out.println(maxCode);
+    Relation[] relationArray = allRelations.toArray(new Relation[allRelations.size()]);
 
-      Relation [] relationArray = allRelations.toArray(new Relation[allRelations.size()]);
+    for (int code = 0; code <= maxCode; code++) {
+      String binCode = int2bin(code, allRelations.size());
 
-      Random rand = new Random();
+      HashSet<Relation> incidentRelations = new HashSet();
+      HashSet<Relation> prefix = new HashSet();
+      ArrayList<Integer> incidentIndexes = new ArrayList();
 
-      for(int code = 0; code<=maxCode; code++){
-        String binCode = int2bin(code, allRelations.size());
-
-        HashSet<Relation> incidentRelations = new HashSet();
-        HashSet<Relation> prefix = new HashSet();
-        ArrayList<Integer> incidentIndexes = new ArrayList();
-
-        for(int i=0; i<binCode.length(); i++)
-        {
-           if(binCode.charAt(i) == '1')
-           {
-             incidentRelations.add(relationArray[i]);
-             incidentIndexes.add(i);
-           }
-
+      for (int i = 0; i < binCode.length(); i++) {
+        if (binCode.charAt(i) == '1') {
+          incidentRelations.add(relationArray[i]);
+          incidentIndexes.add(i);
         }
-
-        for(int i=0; i<binCode.length() && prefix.size() < incidentRelations.size() - 1; i++)
-        {
-           if(binCode.charAt(i) == '1')
-             prefix.add(relationArray[i]);
-        }
-
-        if(reductions.containsKey(incidentRelations))
-          continue;
-        
-        //neighbor boost to add some systematic characteristic to the costs.
-        double boost = 1.0;
-        for (int i = 0; i<incidentIndexes.size()-1 && incidentIndexes.size() > 1; i++)
-        {
-          if (Math.abs(incidentIndexes.get(i) - incidentIndexes.get(i+1)) == 1)
-            boost = boost/(maxTableSize*maxTableSize);//Math.pow(10.0, incidentIndexes.size());
-        }
-
-        if (!reductions.containsKey(prefix) || prefix.size() == 0)
-        { 
-          reductions.put(incidentRelations, 1.0);
-          System.out.println(prefix + " => " + reductions.get(prefix) + " xx " + incidentRelations + " " + reductions.size());
-        }
-        else{
-
-          Double prefixReduction = reductions.get(prefix);
-          
-          System.out.println(prefix + " => " + prefixReduction + " xx " + incidentRelations + " " + reductions.size());
-
-          reductions.put(incidentRelations, prefixReduction*(rand.nextDouble())*boost);
-
-        }
-        
       }
 
-      return reductions;
-  }
+      for (int i = 0; i < binCode.length() && prefix.size() < incidentRelations.size() - 1; i++) {
+        if (binCode.charAt(i) == '1') prefix.add(relationArray[i]);
+      }
 
+      if (reductions.containsKey(incidentRelations)) continue;
+
+      // neighbor boost to add some systematic characteristic to the costs.
+      double boost = 1.0;
+      for (int i = 0; i < incidentIndexes.size() - 1 && incidentIndexes.size() > 1; i++) {
+        if (Math.abs(incidentIndexes.get(i) - incidentIndexes.get(i + 1)) == 1)
+          boost = boost / (maxTableSize * maxTableSize); // Math.pow(10.0, incidentIndexes.size());
+      }
+
+      if (!reductions.containsKey(prefix) || prefix.size() == 0) {
+        reductions.put(incidentRelations, 1.0);
+        System.out.println(
+            prefix
+                + " => "
+                + reductions.get(prefix)
+                + " xx "
+                + incidentRelations
+                + " "
+                + reductions.size());
+      } else {
+
+        Double prefixReduction = reductions.get(prefix);
+
+        System.out.println(
+            prefix
+                + " => "
+                + prefixReduction
+                + " xx "
+                + incidentRelations
+                + " "
+                + reductions.size());
+
+        reductions.put(incidentRelations, prefixReduction * (random.nextDouble()) * boost);
+      }
+    }
+
+    return reductions;
+  }
 
   private Histogram generateColumn(int size, int distinct, int type) {
 
-    //System.out.println("test");
+    // System.out.println("test");
 
-    Random rand = new Random();
     Histogram h;
 
     switch (type) {
@@ -161,74 +174,60 @@ public class DatasetGenerator {
       }
     }
 
-    
-
-    if(type == NUMBER){
+    if (type == NUMBER) {
       ArrayList<Double> weights = generateGaussWeights(distinctIntegerKeys);
 
-      for (int i = 0; i < size; i++)
-        h.put(sample(distinctIntegerKeys, weights)+ 0.0);
-    }
-    else{
+      for (int i = 0; i < size; i++) h.put(sample(distinctIntegerKeys, weights) + 0.0);
+    } else {
 
-      for (int i = 0; i < size; i++)
-        h.put(rand.nextInt(distinctKeys.size()) + 0.0);
-
+      for (int i = 0; i < size; i++) h.put(random.nextInt(distinctKeys.size()) + 0.0);
     }
 
     return h;
   }
 
-  private ArrayList<Double> generateGaussWeights(ArrayList<Integer> nums){
+  private ArrayList<Double> generateGaussWeights(ArrayList<Integer> nums) {
 
     double sum = 0.0;
-    for(Integer i: nums)
-      sum += i + 0.0;
+    for (Integer i : nums) sum += i + 0.0;
 
     double mean = sum / nums.size();
-    double std = maxTableSize/4.0;
+    double std = maxTableSize / 4.0;
 
     ArrayList<Double> unnormalizedWeights = new ArrayList(nums.size());
     ArrayList<Double> weights = new ArrayList(nums.size());
 
     double z = 0.0;
-    for(Integer i: nums)
-    {
-      double x = Math.exp(- Math.pow( (i-mean) , 2 )/std);
+    for (Integer i : nums) {
+      double x = Math.exp(-Math.pow((i - mean), 2) / std);
       z += x;
       unnormalizedWeights.add(x);
     }
 
-    for(Double x: unnormalizedWeights)
-      weights.add(x/z);
+    for (Double x : unnormalizedWeights) weights.add(x / z);
 
     return weights;
   }
 
+  private Integer sample(ArrayList<Integer> nums, ArrayList<Double> weights) {
 
-  private Integer sample(ArrayList<Integer> nums, ArrayList<Double> weights){
+    double randomWeight = random.nextDouble();
 
-    Random rand = new Random();
-    double randomWeight = rand.nextDouble();
-
-    for (int i=0; i < nums.size(); i++){
+    for (int i = 0; i < nums.size(); i++) {
       randomWeight = randomWeight - weights.get(i);
 
-      if (randomWeight <= 0)
-        return nums.get(i);
+      if (randomWeight <= 0) return nums.get(i);
     }
-    
+
     return nums.get(nums.size() - 1);
-
   }
-
 
   private String randomString() {
     return UUID.randomUUID().toString().replace("-", "");
   }
 
   private int randomNumber() {
-    return new Random().nextInt(maxTableSize);
+    return random.nextInt(maxTableSize);
   }
 
   private String randomDate() {
@@ -236,12 +235,10 @@ public class DatasetGenerator {
   }
 
   private LinkedList<Integer> generateAttributes() {
-    Random rand = new Random();
-
     LinkedList<Integer> attrs = new LinkedList();
 
     for (int i = 0; i < numAttributes; i++) {
-      int type = rand.nextInt(3);
+      int type = random.nextInt(3);
       attrs.add(type);
     }
 
@@ -249,46 +246,38 @@ public class DatasetGenerator {
   }
 
   public Relation generateRelation() {
-
-    Random rand = new Random();
-
-    int numAttributesInRel = rand.nextInt(numAttributes) + 2;
+    int numAttributesInRel = random.nextInt(numAttributes) + 2;
 
     HashSet<String> attributes = new HashSet();
 
     for (int j = 0; j < numAttributesInRel; j++) {
-      attributes.add(rand.nextInt(numAttributes) + "");
+      attributes.add(random.nextInt(numAttributes) + "");
     }
 
     return new Relation(attributes.toArray(new String[attributes.size()]));
   }
 
-  private int sampleOrderOfMagnitude(int maxSize, int minSize, double decay){
+  private int sampleOrderOfMagnitude(int maxSize, int minSize, double decay) {
+    while (maxSize >= minSize) { // hyperparam
 
-    Random rand = new Random();
-    while (maxSize >= minSize){ //hyperparam
-      
-      if(rand.nextDouble() > decay)
-        break;
+      if (random.nextDouble() > decay) break;
 
       maxSize /= 10;
     }
 
-    return Math.max(maxSize,minSize);
-
+    return Math.max(maxSize, minSize);
   }
-
 
   public HashMap<Attribute, Histogram> generateData(Relation r) {
 
     HashMap<Attribute, Histogram> data = new HashMap();
 
-    //boolean primary = true;
-    Random rand = new Random();
+    // boolean primary = true;
+    int tableSize =
+        sampleOrderOfMagnitude(
+            maxTableSize, 10, 0.25); // maxTableSize / divisors[rand.nextInt(5)] + 1;
 
-    int tableSize = sampleOrderOfMagnitude(maxTableSize, 10, 0.25); //maxTableSize / divisors[rand.nextInt(5)] + 1;
-
-    //System.out.println(tableSize);
+    // System.out.println(tableSize);
 
     for (String attr : r) {
       int ind = Integer.parseInt(attr);
@@ -301,8 +290,7 @@ public class DatasetGenerator {
 
         int distinct = sampleOrderOfMagnitude(tableSize, 1, 0.25);
 
-        data.put(attrObj,
-            generateColumn(tableSize, distinct, type));
+        data.put(attrObj, generateColumn(tableSize, distinct, type));
       }
 
       data.put(attrObj, generateColumn(tableSize, tableSize, type));
